e49d4102fc7721bc9ffc2f573c43ac75a059f15e
nimrod02 <letrun.a@gmail.com>
2023-09-04 16:44:37 +0200

    add task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module 8
+"""
+
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """
+    Python function that lists all documents in a collection
+    """
+    Collection = collection.find({})
+    return Collection
+
+
