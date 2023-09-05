9a3dd68c913e6ed33615676158f3d843cf71fc01
nimrod02 <letrun.a@gmail.com>
2023-09-05 14:42:31 +0200

    add task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+module 12
+"""
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs.ngix
+
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    count_logs = db.count_documents({})
+    print(f"{count_logs} logs")
+
+    print("Methods:")
+    for method in methods:
+        count_method = db.count_documents({"method": method})
+        print(f"\tmethod {method}: {count_method}")
+
+    check = db.count_documents({"method": "GET", "path": "/status"})
+
+    print(f"{check} status check")
