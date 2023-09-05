ee1533fa3982c6dfc6d8f294345a4c502a3a215c
nimrod02 <letrun.a@gmail.com>
2023-09-05 09:26:22 +0200

    change the name of the variable

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -11,6 +11,6 @@ def insert_school(mongo_collection, **kwargs):
     Args:
         mongo_collection (int): collection of object
     """
-    insert = mongo_collection.insert_one(kwargs)
+    result = mongo_collection.insert_one(kwargs)
 
-    return insert._id
+    return result.inserted_id
