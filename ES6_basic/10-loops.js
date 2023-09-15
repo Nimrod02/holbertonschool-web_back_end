0754164086b66513413f9beafe62448560e7833b
nimrod02 <letrun.a@gmail.com>
2023-09-15 09:51:35 +0200

    fix linter task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,4 +1,5 @@
-export default function appendToEachArrayValue(array) {
+    // eslint-disable-next-line no-unused-expressions
+export default function appendToEachArrayValue(array, appendring) {
   for (const idx of array) {
     array[idx];
   }
