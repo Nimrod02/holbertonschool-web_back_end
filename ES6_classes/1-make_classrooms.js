83645f72cdc70586f23a1a7d541699ef986f9f9e
nimrod02 <letrun.a@gmail.com>
2023-09-15 15:44:09 +0200

    fix linter on task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from "./0-classroom";
 
 export default function initializeRooms() {
-  const classRoom_1 = new ClassRoom(19);
-  const classRoom_2 = new ClassRoom(20);
-  const classRoom_3 = new ClassRoom(34);
+  const classRoom1 = new ClassRoom(19);
+  const classRoom2 = new ClassRoom(20);
+  const classRoom3 = new ClassRoom(34);
 
-  return [classRoom_1, classRoom_2, classRoom_3];
+  return [classRoom1, classRoom2, classRoom3];
 }
