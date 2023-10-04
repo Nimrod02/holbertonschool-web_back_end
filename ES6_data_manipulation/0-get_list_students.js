97f1ed0221c6d4194a76022cc49b75e2b8c3de17
nimrod02 <letrun.a@gmail.com>
2023-10-04 10:54:33 +0200

    refactor task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,8 @@
 export default function getListStudents() {
-  const firstStudent = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-  const secondStudent = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-  const thirdStudent = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-  return [firstStudent, secondStudent, thirdStudent];
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
 }
