1f94a47fdaeec0d58202a2d783fd2ddf9ff68c5e
nimrod02 <letrun.a@gmail.com>
2023-10-06 15:38:12 +0200

    fix task 10 with lint and output

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,10 @@
 function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw error('Cannot process');
+    throw Error('Cannot process');
   }
 
   for (const [key] of map) {
-    if (map.get(key) === 1) map.set(key, 1000);
+    if (map.get(key) === 1) map.set(key, 100);
   }
   return map;
 }
